d9c61d42dc93db00af6458d115f6809f0e5c2ae3
Wesley Casselman <w-casselman@onu.edu>
2023-11-30 08:34:46 -0500

    pressEnterToContinue function added

--- Lab 13A/P13A.cpp
+++ Lab 13A/P13A.cpp
@@ -3,3 +3,23 @@
 	Wesley Casselman
 	November 30th of 2023
 */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void pressEnterToContinue(void);
+
+int main(void)
+{
+	pressEnterToContinue();
+	return 0;
+}
+
+void pressEnterToContinue() {
+	cout << "\nPress enter to continue... \n";
+	cin.clear();
+	while (cin.get() != '\n') continue;
+}
