7f4844e3b1acfef38b34c5b38210b6f9a0add36b
Wesley Casselman <w-casselman@onu.edu>
2023-11-30 09:08:52 -0500

    letterCounts function working

--- Lab 13A/P13A.cpp
+++ Lab 13A/P13A.cpp
@@ -6,20 +6,65 @@
 
 #include <iostream>
 #include <fstream>
-#include <string>
 
 using namespace std;
 
+const int ALPHABET_LETTERS = 26;
+
 void pressEnterToContinue(void);
+void letterCounts(string filename, int* pointer);
 
 int main(void)
 {
+	int letters[ALPHABET_LETTERS];
+	for (int i = 0; i < ALPHABET_LETTERS; i++)
+	{
+		letters[i] = 0;
+	}
+	int* ptr = letters;
+
+	letterCounts("test.txt", ptr);
+
+	for (int i = 0; i < ALPHABET_LETTERS; i++)
+	{
+		cout << letters[i] << "  ";
+	}
+
 	pressEnterToContinue();
 	return 0;
 }
 
 void pressEnterToContinue() {
-	cout << "\nPress enter to continue... \n";
+	cout << "\nPress enter to continue...";
 	cin.clear();
 	while (cin.get() != '\n') continue;
+}
+
+void letterCounts(string filename, int* pointer)
+{
+	ifstream incoming;
+	incoming.open(filename);
+
+	if (!incoming.is_open())
+	{
+		cout << "\nError - file did not open\n";
+		return;
+	}
+
+	while (!incoming.eof())
+	{
+		char ch;
+		incoming >> ch;
+		for (int i = 0; i < ALPHABET_LETTERS; i++)
+		{
+			if (ch == 'a' + i || ch == 'A' + i)
+			{
+				*(pointer + i) += 1;
+				break;
+			}
+		}
+	}
+
+	incoming.close();
+	return;
 }
